bf218d28fe9767b8b3574dbd77c99bbecb18008a
tropicaljazz <l>
2015-02-07 10:16:27 +0000

    Initial Commmit

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,27 @@
+class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all
+  end
+  
+  def show
+    @project = Project.find(params[:id])
+  end
+  
+  def new
+    @project = Project.new
+  end
+  
+  def create 
+    @project = Project.new(project_params)
+    @project.save
+    redirect_to projects_path
+  end
+  
+  private
+    def project_params
+      params[:project].permit(:title)
+    end
+
+
+end
